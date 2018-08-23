e1ea115e9e5386ea822cfc88db30002938e7683e
Nada Gamal Mohamed <nadagamal003@gmail.com>
2018-08-23 15:05:23 +0200

    Add Expanded Room Plan View

--- JazHotels/AppDelegate/AppDelegate.swift
+++ JazHotels/AppDelegate/AppDelegate.swift
@@ -70,14 +70,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UITabBarDelegate, GIDSignI
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
         // ...
-        if let error = error {
+        if error != nil {
             // ...
             return
         }
         
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                       accessToken: authentication.accessToken)
+        guard user.authentication != nil else { return }
+       // let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                     //  accessToken: authentication.accessToken)
         
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: HotelJazConstants.SocialPath.kSocialAuthenticationPathGoogle), object: user)
 
--- JazHotels/ViewControllers/RatePlans/RatePlansViewController.swift
+++ JazHotels/ViewControllers/RatePlans/RatePlansViewController.swift
@@ -13,6 +13,8 @@ class RatePlansViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     var roomRateList :[JCRoomRate] = []
     var ratePlans:JCRatePlan!
+    var isExpanded = false
+    var expandableCells=[Int]()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Rate Plans"
@@ -31,37 +33,69 @@ class RatePlansViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    @objc func headerTapped(tapGesture:UITapGestureRecognizer){
+        isExpanded = true
+        let selectedSection = (tapGesture.view?.tag)!
+        if expandableCells.contains(selectedSection){
+            var selectedIndex = 0
+            for i in (0...expandableCells.count-1){
+                if expandableCells[i] == selectedSection{
+                    selectedIndex = i
+                    break
+                }
+            }
+            expandableCells.remove(at: selectedIndex)
+        }
+        else{
+            expandableCells.append(selectedSection)
+        }
+        tableView.reloadData()
+        
+    }
 }
 
 extension RatePlansViewController :UITableViewDelegate , UITableViewDataSource
 {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "rate_cell") as! RatePlanTableViewCell
-        cell.ratePrice.text = roomRateList[indexPath.row].rates?.rate?.total?.amountAfterTax
-        if ratePlans != nil && ratePlans.ratePlan.count>0{
-        cell.rateTitle.text = ratePlans.ratePlan[0].ratePlanDescription.name ?? ""
-            if ratePlans.ratePlan[0].ratePlanDescription.text != nil{
-                let str = ratePlans.ratePlan[0].ratePlanDescription.text.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
-                cell.rateDesc.text = str
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailsCell") as! RatePlanTableViewCell
 
-            }
-
-        }
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return roomRateList.count
+        if expandableCells.contains(section){
+            return 1
+        }else{
+            return 0
+        }
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         
-        return 1
+        return roomRateList.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        return 180.0
+        return 287.0
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(headerTapped))
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rate_cell") as! RatePlanTableViewCell
+        cell.tag = section
+        cell.ratePrice.text = roomRateList[section].rates?.rate?.total?.amountAfterTax
+        if ratePlans != nil && ratePlans.ratePlan.count>0{
+            cell.rateTitle.text = ratePlans.ratePlan[0].ratePlanDescription.name ?? ""
+            if ratePlans.ratePlan[0].ratePlanDescription.text != nil{
+                let str = ratePlans.ratePlan[0].ratePlanDescription.text.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+                cell.rateDesc.text = str
+                
+            }
+            
+        }
+        cell.addGestureRecognizer(tapGesture)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 180
     }
 }

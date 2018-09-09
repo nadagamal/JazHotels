db072cea52e5739c10dd538204006422cee2f4ab
Nada Gamal Mohamed <nadagamal003@gmail.com>
2018-09-09 18:26:35 +0200

    Add menu and reservations view controllers

---
+++ JazHotels/ViewControllers/MenuViewController/MenuViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MenuViewController.swift
+//  JazHotels
+//
+//  Created by Nada Gamal Mohamed on 9/9/18.
+//  Copyright © 2018 Jaz. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.tableView.tableFooterView = UIView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension MenuViewController:UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell!
+        if indexPath.row == 0{
+            cell = tableView.dequeueReusableCell(withIdentifier: "SignoutCell")
+        }else{
+        cell = tableView.dequeueReusableCell(withIdentifier: "ReservationCell")
+        }
+        return cell
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 1{
+            
+        }
+    }
+}
